f923f7b1595d2d872ffa6c8813b4f322d18c1165
brunobahri <brunobahri27@gmail.com>
2024-08-06 23:21:51 -0300

    chore: configure Sequelize instance

---
+++ sequelize.js
@@ -0,0 +1,9 @@
+// sequelize.js
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: process.env.DB_PATH
+});
+
+module.exports = sequelize;
