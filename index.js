5cd518e2786a95018e1b91a25673ffc2f3da6896
brunobahri <brunobahri27@gmail.com>
2024-08-14 19:53:35 -0300

    refactor(index): ensured database synchronization before starting the server

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-// index.js
-const app = require('./app');
+const { app, sequelize } = require('./app'); // Importa tanto o app quanto a instância do sequelize
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    // Sincronizando os modelos com o banco de dados
+    await sequelize.sync();
+    console.log('Database synchronized.');
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    process.exit(1); // Sai da aplicação em caso de erro na conexão com o banco de dados
+  }
+}
+
+startServer();
